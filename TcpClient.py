588554f61b5e33931df5b8e689ee0e01bd4cfe88
Jack <hgj1995@gmail.com>
2016-03-18 21:00:14 +0800

    fix ipv6

---
+++ TcpClient.py
@@ -0,0 +1,11 @@
+import socket
+
+target_host="vhacker.me"
+target_port="2015"
+
+client=socket.socket(socket.AF_INET6)
+client.connect((target_host,target_port))
+client.send("Hello!")
+response=client.recv(1024)
+print response
+
---
+++ TcpServer.py
@@ -0,0 +1,13 @@
+import socket
+
+bind_ip="2402:9e80:0:1000::1:f3db"
+bind_port=9996
+
+server=socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
+server.bind((bind_ip,bind_port))
+server.listen(5)
+print "[*]Listening on %s:%d" %(bind_ip,bind_port)
+client,addr =server.accept()
+print "[*]Accepted connection from %s:%d" %(addr[0],addr[1])
+request=client.recv(1024)
+print "Recived:%s" % request
