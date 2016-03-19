74e9128227cf562223f5f0cb9b652f5044ed0a6e
Jack <hgj1995@gmail.com>
2016-03-19 17:54:30 +0800

    use thread

--- TcpClient.py
+++ TcpClient.py
@@ -1,11 +1,17 @@
 import socket
 
-target_host="vhacker.me"
+target_host="2001:250:1002:2466:5cc7:3eb1:28b2:e40"
 target_port=2015
 
 client=socket.socket(socket.AF_INET6)
 client.connect((target_host,target_port))
-client.send("Hello!")
-response=client.recv(1024)
+message=raw_input(":")
+while message != "exit":
+    client.send(message)
+    response=client.recv(1024)
+    print response
+    message=raw_input(":")
+client.send(message)    #Let server receive the message to close the sockset
 print response
+
 
--- TcpServer.py
+++ TcpServer.py
@@ -1,13 +1,22 @@
 import socket
-
-bind_ip="2402:9e80:0:1000::1:f3db"
+import threading
+bind_ip="2001:250:1002:2466:5cc7:3eb1:28b2:e40"
 bind_port=2015
+
+def handle_client(client_socket):
+    request=client.recv(1024)
+    while request != "exit":
+        print "Recived:%s" % request
+        client_socket.send("Accpted")
+        request=client.recv(1024)
+   # client_socket.send("See you!") #client has closed
+    client_socket.close()
 
 server=socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
 server.bind((bind_ip,bind_port))
-server.listen(5)
+server.listen(64)
 print "[*]Listening on %s:%d" %(bind_ip,bind_port)
 client,addr =server.accept()
-print "[*]Accepted connection from %s:%d" %(addr[0],addr[1])
-request=client.recv(1024)
-print "Recived:%s" % request
+print "[*]Accepted connection from [%s]:%d" %(addr[0],addr[1])
+client_handler=threading.Thread(target=handle_client,args=(client,))
+client_handler.start()
